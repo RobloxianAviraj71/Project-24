fcbf69056c460e600e259aca28bd705693e35b75
RobloxianAviraj71 <75063755+RobloxianAviraj71@users.noreply.github.com>
2021-03-13 20:33:05 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -7,61 +6,22 @@ const Body = Matter.Body;
 
 function preload()
 {
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
+	
 }
 
 function setup() {
 	createCanvas(800, 700);
-	rectMode(CENTER);
-	
-
-	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
-
-	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
-
-	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
 
 
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
-	World.add(world, packageBody);
-	
-
-	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
-
- 	boxPosition=width/2-100
- 	boxY=610;
-
-
- 	boxleftSprite=createSprite(boxPosition, boxY, 20,100);
- 	boxleftSprite.shapeColor=color(255,0,0);
-
- 	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
- 	World.add(world, boxLeftBody);
-
- 	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
- 	boxBase.shapeColor=color(255,0,0);
-
- 	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
- 	World.add(world, boxBottomBody);
-
- 	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
- 	boxleftSprite.shapeColor=color(255,0,0);
-
- 	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
- 	World.add(world, boxRightBody);
-
-
+	//Create the Bodies Here.
+ground1 = new grounds (400,680,800,30)
+hammer1=new hammer(200,200,100,30)
+rubber1=new rubber(200,200,20)
+stone1=new stone(300,100,100,100)
+iron1=new iron(500,10,130,130)
 	Engine.run(engine);
   
 }
@@ -69,35 +29,16 @@ function setup() {
 
 function draw() {
   rectMode(CENTER);
-  background(0);
- 
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
-
+  background("lightgreen");
+  ground1.display()
+  hammer1.display()
+  rubber1.display()
+stone1.display()
+iron1.display()
   
+
   drawSprites();
-  
-  
  
-}
-
-function keyPressed() {
-  if (keyCode === LEFT_ARROW) {
-
-    helicopterSprite.x=helicopterSprite.x-20;    
-    translation={x:-20,y:0}
-    Matter.Body.translate(packageBody, translation)
-
-
-  } else if (keyCode === RIGHT_ARROW) {
-    helicopterSprite.x=helicopterSprite.x+20;
-    translation={x:20,y:0}
-    Matter.Body.translate(packageBody, translation)
-  }
-  else if (keyCode === DOWN_ARROW) {
-    Matter.Body.setStatic(packageBody,false);
-    
-  }
 }
 
 
